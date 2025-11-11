70a602aa58c6f51086e6280c5d22b82f21b73b72
Christopher Zalapa <63930010+ChristopherZalapa@users.noreply.github.com>
2025-11-11 09:43:42 -0800

    Added data file

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import http from "node:http";
+import data from "./Data/data.js";
 
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-	res.end("Hello from the server");
+	if (req.url === "/api" && req.method === "GET") {
+		res.end("Hello from the server");
+	}
 });
+
+const destinations = data;
+
+console.log(destinations);
 
 server.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
